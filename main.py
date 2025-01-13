04439fe7b9d277e16753301c48ca8dc1f12693ca
Sibyrd <mail@theobird.com>
2025-01-13 16:46:03 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 santadb = []
 
 def checkName(name):
@@ -6,6 +8,18 @@ def checkName(name):
             print("This name has already been entered.")
             return True
     return False
+
+def assignPerson():
+    possible = []
+    for i in range(0,len(santadb)):
+        possible.append(i)
+    for x in santadb:
+        randchoice = randint(0,len(possible)-1)
+        x.append(possible[randchoice])
+        possible.pop(randchoice)
+    print("🎅 Here are your pairings: ")
+    for x in santadb:
+        print(x[0] + " gives a gift to " + santadb[x[1]][0])
 
 while True:
     name = input("Enter name to be added (enter stop to stop): ")
@@ -16,4 +30,5 @@ while True:
     else:
         santadb.append([name])
 
-print(santadb)
+
+assignPerson()
