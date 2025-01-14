2cd36c1dccdd7e3fb5f9187cec3e47cb2c4c3045
Sibyrd <mail@theobird.com>
2025-01-14 09:47:31 +0000

    file

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ def checkName(name):
     return False
 
 def assignPerson():
-    possible = []
-    for i in range(0,len(santadb)):
-        possible.append(i)
-    for x in santadb:
-        randchoice = randint(0,len(possible)-1)
-        x.append(possible[randchoice])
-        possible.pop(randchoice)
-    print("🎅 Here are your pairings: ")
-    for x in santadb:
-        print(x[0] + " gives a gift to " + santadb[x[1]][0])
+    with open("results.txt", "w") as file:
+        possible = []
+        total = ""
+        for i in range(0,len(santadb)):
+            possible.append(i)
+        for x in santadb:
+            randchoice = randint(0,len(possible)-1)
+            x.append(possible[randchoice])
+            possible.pop(randchoice)
+        print("🎅 Here are your pairings: ")
+        for x in santadb:
+            string = x[0] + " gives a gift to " + santadb[x[1]][0]
+            print(string)
+            total = total+f"\n{string}"
+        file.write(total)
+        file.close()
 
 while True:
     name = input("Enter name to be added (enter stop to stop): ")
